70ce0d48021836373e104d3498577c025f199eb3
Fabio <vavarl1@gmail.com>
2017-05-09 00:09:59 -0300

    Ajuste Frequencia

--- app/webroot/js/page.frequencias.js
+++ app/webroot/js/page.frequencias.js
@@ -2,4 +2,8 @@ jQuery(document).ready(function() {
     $('.change-disciplina').on('click', function(event){
         $('#FrequenciaDisciplinaId').val($(this).attr('href').replace("#", ""));
     });
+
+    $('.accordion').accordion({
+      heightStyle: "content"
+    });
 });
